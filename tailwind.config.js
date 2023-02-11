c99a1280800e9933fac740a8a82c20c605622f72
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-11 11:09:39 -0700

    tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
@@ -9,7 +8,6 @@ module.exports = {
       xs: "540px",
       md: "740px",
       lg: "1040px",
-      // ...defaultTheme.screens,
     },
     extend: {},
   },
